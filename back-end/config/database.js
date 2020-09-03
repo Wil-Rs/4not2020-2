e5729191915d509fa24731eb08dc0009f0e3f5d3
Wil-Rs <wilianrsantos.pro@gmail.com>
2020-09-03 19:26:18 +0000

    metade da aula que faltei

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -5,4 +5,21 @@ module.exports = uri => {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
+    // Quando conectado com sucesso imprime essa menssagem no console...
+    mongoose.connection.on('connected', () => {
+        console.log(' =======> Mongoose! Conectado com sucesso ao servidor...')
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('\n\n\n ===> Mongoose! Desconectado do servidor...')
+    })
+
+    // Captura um sinal de encerramento (SIGIT), ctrl + c
+    process.on('SIGINT', () => {
+        mongoose.connection.close( () => {
+            console.log(' **** Mongoose! Desconectado pelo termino da aplicacao.... ****')
+            // 0 indica que a finalizacao ocorreu sem erros
+            process.exit(0)
+        })
+    })
 }
